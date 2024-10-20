418d0cc1c46a50c1fe0519da1d261f6d29b1e483
qafulov <qafulovh@gmail.com>
2024-10-20 22:07:24 +0400

    Added new functionality and desing to header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   const handleScroll = () => {
     const sections = document.querySelectorAll("section");
-    const scrollPosition = window.scrollY + 100; // Scroll pozisyonunu ayarlayın
+    const scrollPosition = window.scrollY + 100;
 
     sections.forEach((section) => {
       if (
@@ -29,127 +29,83 @@ const Header = () => {
     };
   }, []);
 
+  // Sayfa yenileme fonksiyonu
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
   return (
     <header className="bg-gray-800 text-white p-4 fixed w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
-        {/* Uygulama İkonu - En Solda */}
-        <div className="flex items-center">
-          <Image
-            src="/icons/yolu.svg" // SVG ikonunun yolu
-            alt="Yolu Icon"
-            width={46}
-            height={46}
-          />
+        {/* Yolu ikonu, tıklandığında sayfayı yeniler */}
+        <div className="header-icon flex items-center" onClick={refreshPage}>
+          <Image src="/icons/yolu.svg" alt="Yolu Icon" width={46} height={46} />
         </div>
-        {/* Navigasyon Linkleri (Desktop için) */}
-        <nav className="hidden md:flex space-x-4 ml-auto">
+
+        {/* Masaüstü Menüsü - %50 genişlikte sınırlandırıldı */}
+        <nav className="hidden md:flex space-x-4 ml-auto max-w-[50%] w-full justify-end">
           <Link
             href="#intro"
             className={`relative hover:text-blue-400 ${
               activeSection === "intro" ? "text-blue-400 font-bold" : ""
-            } text-lg`}
+            }`}
           >
             Giriş
-            <span
-              className={`absolute bottom-0 left-0 right-0 h-[2px] bg-blue-400 transition-all duration-300 ${
-                activeSection === "intro" ? "w-full" : "w-0"
-              }`}
-            ></span>
           </Link>
           <Link
             href="#features"
             className={`relative hover:text-blue-400 ${
               activeSection === "features" ? "text-blue-400 font-bold" : ""
-            } text-lg`}
+            }`}
           >
             Özellikler
-            <span
-              className={`absolute bottom-0 left-0 right-0 h-[2px] bg-blue-400 transition-all duration-300 ${
-                activeSection === "features" ? "w-full" : "w-0"
-              }`}
-            ></span>
           </Link>
           <Link
             href="#about"
             className={`relative hover:text-blue-400 ${
               activeSection === "about" ? "text-blue-400 font-bold" : ""
-            } text-lg`}
+            }`}
           >
             Hakkımızda
-            <span
-              className={`absolute bottom-0 left-0 right-0 h-[2px] bg-blue-400 transition-all duration-300 ${
-                activeSection === "about" ? "w-full" : "w-0"
-              }`}
-            ></span>
           </Link>
           <Link
             href="#download"
             className={`relative hover:text-blue-400 ${
               activeSection === "download" ? "text-blue-400 font-bold" : ""
-            } text-lg`}
+            }`}
           >
             İndir
-            <span
-              className={`absolute bottom-0 left-0 right-0 h-[2px] bg-blue-400 transition-all duration-300 ${
-                activeSection === "download" ? "w-full" : "w-0"
-              }`}
-            ></span>
           </Link>
         </nav>
 
-        {/* Mobil Menü Düğmesi */}
+        {/* Mobil Menüsü */}
         <div className="md:hidden">
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="text-white focus:outline-none"
+            className="menu-toggle text-white focus:outline-none"
           >
-            {isMenuOpen ? "X" : <span>&#9776;</span>} {/* Hamburger iconu */}
+            {isMenuOpen ? "X" : <span>&#9776;</span>}
           </button>
         </div>
       </div>
-      {/* Dropdown Menü (Mobil için) */}
+
+      {/* Dropdown Menü - Mobil İçin */}
       <nav
         className={`md:hidden ${
           isMenuOpen ? "block" : "hidden"
         } bg-gray-800 p-4`}
       >
         <div className="flex flex-col items-center space-y-2">
-          {" "}
-          {/* items-center eklendi */}
-          <Link
-            href="#intro"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "intro" ? "text-blue-400 font-bold" : ""
-            } text-base`}
-            onClick={() => setIsMenuOpen(false)} // Menüyü kapat
-          >
+          <Link href="#intro" onClick={() => setIsMenuOpen(false)}>
             Giriş
           </Link>
-          <Link
-            href="#features"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "features" ? "text-blue-400 font-bold" : ""
-            } text-base`}
-            onClick={() => setIsMenuOpen(false)} // Menüyü kapat
-          >
+          <Link href="#features" onClick={() => setIsMenuOpen(false)}>
             Özellikler
           </Link>
-          <Link
-            href="#about"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "about" ? "text-blue-400 font-bold" : ""
-            } text-base`}
-            onClick={() => setIsMenuOpen(false)} // Menüyü kapat
-          >
+          <Link href="#about" onClick={() => setIsMenuOpen(false)}>
             Hakkımızda
           </Link>
-          <Link
-            href="#download"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "download" ? "text-blue-400 font-bold" : ""
-            } text-base`}
-            onClick={() => setIsMenuOpen(false)} // Menüyü kapat
-          >
+          <Link href="#download" onClick={() => setIsMenuOpen(false)}>
             İndir
           </Link>
         </div>
--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -2,19 +2,16 @@ import Image from "next/image";
 
 const Intro = () => {
   return (
-    <section
-      id="intro"
-      className="bg-gray-100 min-h-screen flex items-center" // Dikey ortalama için "flex" ve "items-center" eklendi
-    >
+    <section id="intro" className="bg-gray-100 min-h-screen flex items-center">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
         {/* Sol Taraf (Yazılar) */}
         <div className="md:w-1/2 text-center md:text-left mb-8 md:mb-0">
           {/* Başlık */}
-          <h1 className="text-5xl font-bold text-gray-800 mb-6">
+          <h1 className="font-bold text-gray-800 mb-6">
             Yolu ile Kariyerinizde Hızla İlerleyin
           </h1>
           {/* Kısa Açıklama */}
-          <p className="text-lg text-gray-600 max-w-2xl mx-auto mb-8">
+          <p className=" text-gray-600 max-w-2xl mx-auto mb-8">
             Yolu, iş arama ve kariyer geliştirme konusunda yenilikçi çözümler
             sunar. Yolu ile iş bulmak, iş ilanı vermek ve kariyerinizde yeni
             fırsatlar keşfetmek daha kolay.
@@ -22,10 +19,10 @@ const Intro = () => {
           {/* İndir Butonu */}
           <div className="flex justify-center md:justify-start">
             <a
-              href="https://play.google.com/store/apps/details?id=com.yolu.app" // Buraya yönlendirmek istediğiniz URL'yi ekleyin
-              target="_blank" // Yeni sekmede açılması için
-              rel="noopener noreferrer" // Güvenlik önlemi
-              className="bg-blue-500 text-white rounded-full px-20 py-5 font-semibold text-xl hover:bg-blue-600 transition"
+              href="https://play.google.com/store/apps/details?id=com.yolu.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-blue-500 text-white rounded-full font-semibold  hover:bg-blue-600 transition downloadButton"
             >
               İndir
             </a>
@@ -35,11 +32,11 @@ const Intro = () => {
         {/* Sağ Taraf (Görsel) */}
         <div className="md:w-1/2 flex justify-center">
           <Image
-            src="/icons/workers.svg" // Uygulama ekran görüntüsü burada olacak
+            src="/icons/workers.svg"
             alt="Uygulama Ekran Görüntüsü"
-            width={360}
-            height={360}
-            className="object-contain"
+            width={400}
+            height={400}
+            className="object-contain w-[220px] h-[220px] md:w-[400px] md:h-[400px]" // Tailwind sınıflarıyla boyutlandırma
           />
         </div>
       </div>
