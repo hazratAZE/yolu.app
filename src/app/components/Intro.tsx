1682535ad34cb54febe66a0b8feeae250ec29ca3
qafulov <qafulovh@gmail.com>
2024-10-21 13:50:01 +0400

    New illustration added

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -5,24 +5,24 @@ const Intro = () => {
     <section id="intro" className="bg-gray-100 min-h-screen flex items-center">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
         {/* Mobilde Yukarıda Olacak Görsel */}
-        <div className="md:w-1/2 flex justify-center order-1 md:order-2 mb-8 md:mb-0">
+        <div className="md:w-1/2 flex justify-center order-1 md:order-2 mb-6 md:mb-0">
           <Image
             src="/icons/workers.svg"
             alt="Uygulama Ekran Görüntüsü"
-            width={400}
-            height={400}
-            className="object-contain w-[200px] h-[200px] md:w-[400px] md:h-[400px]" // Tailwind sınıflarıyla boyutlandırma
+            width={420}
+            height={420}
+            className="object-contain w-[220px] h-[220px] md:w-[420px] md:h-[420px]" // Tailwind sınıflarıyla boyutlandırma
           />
         </div>
 
         {/* Sol Taraf (Yazılar) */}
         <div className="md:w-1/2 text-center md:text-left order-2 md:order-1">
           {/* Başlık */}
-          <h1 className="font-bold text-gray-800 mb-6">
+          <h1 className="font-bold text-gray-800 mb-4">
             Yolu ilə Karyeranızda Sürətlə İrələyin
           </h1>
           {/* Kısa Açıklama */}
-          <p className="text-gray-600 max-w-2xl mx-auto mb-8">
+          <p className="text-gray-600 max-w-2xl mx-auto mb-6">
             Yolu, iş axtarışı və karyera inkişafı ilə bağlı yenilikçi həllər
             təqdim edir. Yolu ilə iş tapmaq, iş elanları vermək və karyeranızda
             yeni fürsətlər kəşf etmək daha asandır.
