9ae00202399b9e76fa92d0443af985bca64960cb
qafulov <qafulovh@gmail.com>
2024-10-21 16:57:22 +0400

    Change padding in about screen

--- src/app/components/Hakkimizda.tsx
+++ src/app/components/Hakkimizda.tsx
@@ -2,51 +2,16 @@ const Hakkımızda = () => {
   return (
     <section
       id="about"
-      className="min-h-screen flex flex-col items-center justify-center p-6"
+      className="min-h-screen flex flex-col items-center justify-center py-4"
     >
       <h1 className="font-bold mb-4 text-black">Hakkımızda</h1>
-      <p className="text-center mb-6 text-gray-600 w-[80%]">
+      <p className="text-center mb-8 text-gray-600 w-[80%]">
         Yolu, iş dünyasında inqilab yaradan yenilikçi bir platformdur. Biz, iş
         axtarışı və karyera inkişafı ilə bağlı istifadəçilərimizin bütün
         ehtiyaclarını qarşılayırıq. Yolu ilə yeni fürsətləri kəşf edin,
         kariyeranızı bir addım irəliləyin!
       </p>
-
-      {/* Vizyon ve Missiya Bölümü */}
-      <div className="flex flex-col md:flex-row mb-8">
-        <div className="md:w-1/2 p-4">
-          <h2 className="text-2xl font-semibold mb-2 text-blue-800">
-            Vizyonumuz
-          </h2>
-          <p className="mx-auto text-gray-800 mb-4">
-            İş dünyasında lider olmağı, müştərilərimizin gözləntilərini aşmağı
-            və karyera inkişafında inqilab etməyi hədəfləyirik.
-          </p>
-          <img
-            src="/images/mission.jpg" // Vizyon resminin yolu
-            alt="Vizyonumuz"
-            className="w-full h-80 object-cover mb-2 rounded-lg"
-          />
-        </div>
-        <div className="md:w-1/2 p-4">
-          <h2 className="text-2xl font-semibold mb-2 text-blue-800">
-            Missiyamız
-          </h2>
-          <p className="mx-auto text-gray-800 mb-4">
-            İstifadəçilərimizə iş tapma, karyera inkişafı və iş elanları vermə
-            sahəsində mükəmməl xidmətlər təqdim edərək onlara yeni fürsətlər
-            yaratmaq.
-          </p>
-          <img
-            src="/images/worker.jpg" // Missiya resminin yolu
-            alt="Missiyamız"
-            className="w-full h-80 object-cover mb-2 rounded-lg"
-          />
-        </div>
-      </div>
-
-      {/* Değerler Bölümü */}
-      <div className="flex flex-col mb-8">
+      <div className="flex flex-col mb-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {/* Her bir değer için dışarıdan yüklenen resimler */}
           <div className="flex items-center text-left p-4 bg-gray-100 rounded-lg">
@@ -87,6 +52,38 @@ const Hakkımızda = () => {
               Komanda İşbirliyi
             </p>
           </div>
+        </div>
+      </div>
+      {/* Vizyon ve Missiya Bölümü */}
+      <div className="flex flex-col md:flex-row mb-8">
+        <div className="md:w-1/2 p-4">
+          <h2 className="text-2xl font-semibold mb-2 text-blue-800">
+            Vizyonumuz
+          </h2>
+          <p className="mx-auto text-gray-800 mb-4">
+            İş dünyasında lider olmağı, müştərilərimizin gözləntilərini aşmağı
+            və karyera inkişafında inqilab etməyi hədəfləyirik.
+          </p>
+          <img
+            src="/images/mission.jpg" // Vizyon resminin yolu
+            alt="Vizyonumuz"
+            className="w-full h-80 object-cover mb-2 rounded-lg"
+          />
+        </div>
+        <div className="md:w-1/2 p-4">
+          <h2 className="text-2xl font-semibold mb-2 text-blue-800">
+            Missiyamız
+          </h2>
+          <p className="mx-auto text-gray-800 mb-4">
+            İstifadəçilərimizə iş tapma, karyera inkişafı və iş elanları vermə
+            sahəsində mükəmməl xidmətlər təqdim edərək onlara yeni fürsətlər
+            yaratmaq.
+          </p>
+          <img
+            src="/images/worker.jpg" // Missiya resminin yolu
+            alt="Missiyamız"
+            className="w-full h-80 object-cover mb-2 rounded-lg"
+          />
         </div>
       </div>
 
