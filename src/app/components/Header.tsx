e1b4521fed19d0eb4eb61cf727127e33658f2a60
qafulov <qafulovh@gmail.com>
2024-10-23 12:42:51 +0400

    Added lang dropdown to header section

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,9 +4,22 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+// Dil türü belirliyoruz
+type Language = "AZ" | "EN" | "TR" | "RU";
+
 const Header = () => {
   const [activeSection, setActiveSection] = useState("intro");
   const [isMenuOpen, setIsMenuOpen] = useState(false); // Menü durumu
+  const [isLangDropdownOpen, setIsLangDropdownOpen] = useState(false); // Dil dropdown durumu
+  const [selectedLanguage, setSelectedLanguage] = useState<Language>("AZ"); // Varsayılan dil
+
+  // Bayrak dosyaları eşlemesi
+  const flagMap = {
+    AZ: "/icons/az-flag.svg",
+    EN: "/icons/uk-flag.svg",
+    TR: "/icons/tr-flag.svg",
+    RU: "/icons/ru-flag.svg",
+  };
 
   const handleScroll = () => {
     const sections = document.querySelectorAll("section");
@@ -34,50 +47,132 @@ const Header = () => {
     window.location.reload();
   };
 
+  // Dil değişimi fonksiyonu (TypeScript uyumlu)
+  const changeLanguage = (lang: Language) => {
+    setSelectedLanguage(lang);
+    setIsLangDropdownOpen(false);
+    console.log("Dil değişti:", lang); // Dil değiştirme işlemi burada yapılabilir
+  };
+
   return (
     <header className="bg-gray-800 text-white p-4 fixed w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
-        {/* Yolu ikonu, tıklandığında sayfayı yeniler */}
+        {/* Yolu ikonu, en solda sabit */}
         <div className="header-icon flex items-center" onClick={refreshPage}>
           <Image src="/icons/yolu.svg" alt="Yolu Icon" width={46} height={46} />
         </div>
 
-        {/* Masaüstü Menüsü - %50 genişlikte sınırlandırıldı */}
-        <nav className="hidden md:flex space-x-4 ml-auto max-w-[50%] w-full justify-end">
-          <Link
-            href="#intro"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "intro" ? "text-blue-400 font-bold" : ""
-            }`}
-          >
-            Giriş
-          </Link>
-          <Link
-            href="#about"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "about" ? "text-blue-400 font-bold" : ""
-            }`}
-          >
-            Haqqında
-          </Link>
-          <Link
-            href="#features"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "features" ? "text-blue-400 font-bold" : ""
-            }`}
-          >
-            Xüsusiyyətlər
-          </Link>
+        {/* Masaüstü Menüsü ve Dil Dropdown */}
+        <div className="flex items-center ml-auto space-x-4 max-w-[50%] w-full justify-end">
+          <nav className="hidden md:flex space-x-4">
+            <Link
+              href="#intro"
+              className={`relative hover:text-blue-400 ${
+                activeSection === "intro" ? "text-blue-400 font-bold" : ""
+              }`}
+            >
+              Giriş
+            </Link>
+            <Link
+              href="#about"
+              className={`relative hover:text-blue-400 ${
+                activeSection === "about" ? "text-blue-400 font-bold" : ""
+              }`}
+            >
+              Haqqında
+            </Link>
+            <Link
+              href="#features"
+              className={`relative hover:text-blue-400 ${
+                activeSection === "features" ? "text-blue-400 font-bold" : ""
+              }`}
+            >
+              Xüsusiyyətlər
+            </Link>
+            <Link
+              href="#download"
+              className={`relative hover:text-blue-400 ${
+                activeSection === "download" ? "text-blue-400 font-bold" : ""
+              }`}
+            >
+              Yüklə
+            </Link>
 
-          <Link
-            href="#download"
-            className={`relative hover:text-blue-400 ${
-              activeSection === "download" ? "text-blue-400 font-bold" : ""
-            }`}
-          >
-            Yüklə
-          </Link>
-        </nav>
+            {/* Dil Dropdown */}
+            <div className="relative">
+              <button
+                className="flex items-center text-white focus:outline-none"
+                onClick={() => setIsLangDropdownOpen(!isLangDropdownOpen)}
+              >
+                {/* Seçilen dil ve bayrak */}
+                <Image
+                  src={flagMap[selectedLanguage]}
+                  alt={`${selectedLanguage} Flag`}
+                  width={20}
+                  height={20}
+                  className="mr-2"
+                />
+                {selectedLanguage} <span className="ml-2">&#9662;</span>
+              </button>
+              {isLangDropdownOpen && (
+                <ul className="absolute right-0 mt-2 bg-gray-700 text-white py-2 w-32 shadow-lg rounded">
+                  <li
+                    className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                    onClick={() => changeLanguage("AZ")}
+                  >
+                    <Image
+                      src={flagMap.AZ}
+                      alt="Azerbaijan Flag"
+                      width={20}
+                      height={20}
+                      className="mr-2"
+                    />
+                    AZ
+                  </li>
+                  <li
+                    className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                    onClick={() => changeLanguage("EN")}
+                  >
+                    <Image
+                      src={flagMap.EN}
+                      alt="UK Flag"
+                      width={20}
+                      height={20}
+                      className="mr-2"
+                    />
+                    EN
+                  </li>
+                  <li
+                    className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                    onClick={() => changeLanguage("TR")}
+                  >
+                    <Image
+                      src={flagMap.TR}
+                      alt="Turkey Flag"
+                      width={20}
+                      height={20}
+                      className="mr-2"
+                    />
+                    TR
+                  </li>
+                  <li
+                    className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                    onClick={() => changeLanguage("RU")}
+                  >
+                    <Image
+                      src={flagMap.RU}
+                      alt="Russia Flag"
+                      width={20}
+                      height={20}
+                      className="mr-2"
+                    />
+                    RU
+                  </li>
+                </ul>
+              )}
+            </div>
+          </nav>
+        </div>
 
         {/* Mobil Menüsü */}
         <div className="md:hidden">
@@ -124,7 +219,6 @@ const Header = () => {
           >
             Xüsusiyyətlər
           </Link>
-
           <Link
             href="#download"
             onClick={() => setIsMenuOpen(false)}
@@ -134,6 +228,80 @@ const Header = () => {
           >
             Yüklə
           </Link>
+
+          {/* Dil Dropdown - Mobilde de mevcut */}
+          <div className="relative">
+            <button
+              className="flex items-center text-white focus:outline-none"
+              onClick={() => setIsLangDropdownOpen(!isLangDropdownOpen)}
+            >
+              {/* Seçilen dil ve bayrak mobilde de görünsün */}
+              <Image
+                src={flagMap[selectedLanguage]}
+                alt={`${selectedLanguage} Flag`}
+                width={20}
+                height={20}
+                className="mr-2"
+              />
+              {selectedLanguage} <span className="ml-2">&#9662;</span>
+            </button>
+            {isLangDropdownOpen && (
+              <ul className="absolute right-0 mt-2 bg-gray-700 text-white py-2 w-32 shadow-lg rounded">
+                <li
+                  className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                  onClick={() => changeLanguage("AZ")}
+                >
+                  <Image
+                    src={flagMap.AZ}
+                    alt="Azerbaijan Flag"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  AZ
+                </li>
+                <li
+                  className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                  onClick={() => changeLanguage("EN")}
+                >
+                  <Image
+                    src={flagMap.EN}
+                    alt="UK Flag"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  EN
+                </li>
+                <li
+                  className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                  onClick={() => changeLanguage("TR")}
+                >
+                  <Image
+                    src={flagMap.TR}
+                    alt="Turkey Flag"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  TR
+                </li>
+                <li
+                  className="px-4 py-2 flex items-center hover:bg-gray-600 cursor-pointer"
+                  onClick={() => changeLanguage("RU")}
+                >
+                  <Image
+                    src={flagMap.RU}
+                    alt="Russia Flag"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  RU
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </nav>
     </header>
