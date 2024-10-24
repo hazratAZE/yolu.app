764aa82b68ebbff3b5c13a5903e1240d0acdf77d
qafulov <qafulovh@gmail.com>
2024-10-24 14:07:08 +0400

    Features screen translates added successfully

--- src/app/components/Ozellikler.tsx
+++ src/app/components/Ozellikler.tsx
@@ -1,30 +1,32 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 const Ozellikler = () => {
+  const t = useTranslations();
   const featuresData = [
     {
       icon: "/icons/map.svg",
-      text: "Xəritə üzərində görsənmək imkanı ilə həm iş elanlarını, həm də digər istifadəçiləri asanlıqla tapın. Beləliklə, iş tapmaq və ya iş elanınızı ön plana çıxarmaq çox daha sürətli və effektiv olacaq. Bu xüsusiyyət sizə zaman qazandıraraq işlərinizin daha asan həll olunmasına kömək edəcək.",
+      text: t("map_feature_desc"),
     },
     {
       icon: "/icons/qr.svg",
-      text: "QR apply xüsusiyyəti ilə işlərə tez və rahat şəkildə müraciət edin. CV hazırlamaqla vaxt itirmədən sadəcə QR kodu ilə müraciət etmə imkanı əldə edərək iş tapma prosesinizi sürətləndirin. Müraciətlərinizi sürətli və problemsiz şəkildə yönləndirmək üçün bu funksiyadan faydalanın.",
+      text: t("qr_apply_feature_desc"),
     },
     {
       icon: "/icons/card.svg",
-      text: "Yolu virtual kartı ilə token hesabınızı asanlıqla idarə edin. Virtual kart əldə edərək, Yolu platformasının bütün funksiyalarından yararlanın. Bu kart vasitəsilə ödəmələrdən tutmuş müxtəlif əməliyyatlara qədər çoxsaylı imkanlardan istifadə edin.",
+      text: t("virtual_card_feature_desc"),
     },
     {
       icon: "/icons/token.svg",
-      text: "Yolu tətbiqi daxilində istifadə olunan token ilə bütün əməliyyatlarda rahatlıqla istifadə edə biləcəyiniz bir tokenə sahib olun. İş müraciətləri, ödənişlər və digər əməliyyatlarda bu tokenin funksionallığı ilə işlərinizi sürətləndirin.",
+      text: t("token_feature_desc"),
     },
     {
       icon: "/icons/earn.svg",
-      text: "Sizə məxsus tokenləri istənilən vaxt sataraq real pula çevirmək imkanı. Tokenlərinizin dəyərini artırmaq və onları pul ekvivalentinə çevirmək üçün sadə və rahat bir yol təklif edirik. Bu yolla qazancınızı artırmaq üçün bu funksiyanı kəşf edin.",
+      text: t("earn_token_feature_desc"),
     },
     {
       icon: "/icons/partner.svg",
-      text: "Bizimlə tərəfdaş olaraq satışlarınızı artırın və daha çox qazanın. Partnyorlarımızla əməkdaşlıq edərək iş imkanlarınızı genişləndirin və yeni müştərilərə çataraq gəlirinizi artırın. Bu əməkdaşlıq sayəsində siz də qazanc əldə edə bilərsiniz.",
+      text: t("partner_feature_desc"),
     },
   ];
 
@@ -34,12 +36,10 @@ const Ozellikler = () => {
       className="bg-gray-100 min-h-screen flex flex-col justify-center pt-16"
     >
       <div className="container mx-auto px-4">
-        <h1 className="text-center text-black font-bold">Xüsusiyyətlər</h1>
+        <h1 className="text-center text-black font-bold">{t("features")}</h1>
         <div className="text-center justify-center flex mt-4">
           <p className="text-gray-600 mb-8 w-[80%]">
-            Yolu platformasının təqdim etdiyi daha çox unikal xüsusiyyətlərdən
-            faydalanmaq və iş dünyasında rəqiblərinizdən bir addım öndə olmaq
-            üçün bizə qoşulun!
+            {t("unique_features_desc")}
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
