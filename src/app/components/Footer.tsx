3192c7afd96335057da47e9990561032e40786ca
qafulov <qafulovh@gmail.com>
2024-10-24 15:36:00 +0400

    Desing change in footer section

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,11 @@ const Footer = () => {
     <footer className="text-white py-6 mt-20 pt-20 bg-gray-800 w-full">
       <div className="container mx-auto flex flex-col md:flex-row justify-center items-center mb-12">
         {/* Sosyal Medya İkonları */}
-        <div className="flex justify-center space-x-6 mb-4 md:mb-0 sm:mb-12">
+        <div className="text-center flex flex-col md:ml-12 mb-4 md:mb-0 sm:mb-8">
+          <p className="text-md">{t("email")}: info@yolu.app</p>
+          <p className="text-md">{t("phone")}: +994 55 374 98 01</p>
+        </div>
+        <div className="flex justify-center space-x-6 ">
           <a
             href="https://www.linkedin.com/company/101312119/admin/feed/posts/?feedType=following"
             target="_blank"
@@ -68,12 +72,6 @@ const Footer = () => {
               className="w-6 h-6 hover:opacity-80 transition-opacity"
             />
           </a>
-        </div>
-
-        {/* İletişim Bilgileri */}
-        <div className="text-center mb-4 md:mb-0 flex flex-col md:ml-12">
-          <p className="text-md">{t("email")}: info@yolu.app</p>
-          <p className="text-md">{t("phone")}: +994 55 374 98 01</p>
         </div>
       </div>
 
