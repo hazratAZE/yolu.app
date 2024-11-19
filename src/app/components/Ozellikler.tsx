5406cd9340016f90939c2392fdc0d7302d2064e4
qafulov <qafulovh@gmail.com>
2024-11-19 19:27:23 +0400

    New desing for fetures

--- src/app/components/Ozellikler.tsx
+++ src/app/components/Ozellikler.tsx
@@ -3,69 +3,91 @@ import Image from "next/image";
 
 const Ozellikler = () => {
   const t = useTranslations();
+
   const featuresData = [
     {
       icon: "/icons/map.svg",
+      title: t("map_feature_title"),
       text: t("map_feature_desc"),
+      color: "darkred",
     },
     {
       icon: "/icons/qr.svg",
+      title: t("qr_apply_feature_title"),
       text: t("qr_apply_feature_desc"),
+      color: "darkorange",
     },
     {
       icon: "/icons/card.svg",
+      title: t("virtual_card_feature_title"),
       text: t("virtual_card_feature_desc"),
+      color: "darkblue",
     },
     {
       icon: "/icons/token.svg",
+      title: t("token_feature_title"),
       text: t("token_feature_desc"),
+      color: "purple",
     },
     {
       icon: "/icons/earn.svg",
+      title: t("earn_token_feature_title"),
       text: t("earn_token_feature_desc"),
+      color: "darkgreen",
     },
     {
       icon: "/icons/partner.svg",
+      title: t("partner_feature_title"),
       text: t("partner_feature_desc"),
+      color: "steelblue",
     },
   ];
 
   return (
     <section
       id="features"
-      className="bg-gray-100 min-h-screen flex flex-col justify-center pt-16 px-4"
+      className="min-h-screen flex flex-col items-center bg-blue-50 px-4 md:px-16 lg:px-32 pt-16 font-sans"
     >
-      <div className="container mx-auto">
-        <h1 className="text-center text-black font-bold">{t("features")}</h1>
-        <div className="text-center justify-center flex mt-4">
-          <p className="text-gray-600 mb-8 md:w-[80%]">
-            {t("unique_features_desc")}
-          </p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {featuresData.map((feature, index) => (
-            <div
-              key={index}
-              className="flex items-center bg-gray-800 p-6 rounded-lg"
-            >
-              <div className="mr-6 shrink-0">
-                {" "}
-                {/* İkonun küçülmesini engelleyen sınıf */}
-                <Image
-                  src={feature.icon}
-                  alt={`Feature ${index + 1}`}
-                  width={46} // İkon boyutunu artırdık ve sabitledik
-                  height={46}
-                  className="object-contain"
-                />
+      {/* Başlık */}
+      <h1 className="text-black font-bold text-center mb-12">
+        {t("features")}
+      </h1>
+
+      <div className="flex flex-col md:flex-row md:items-center w-full gap-12">
+        {/* Sol taraf: Özellikler */}
+        <div className="flex flex-col flex-1 order-2 md:order-2">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            {featuresData.map((feature, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-start justify-start text-left border-l-8 border-gray-600 pl-8 p-4"
+              >
+                <div
+                  className="mb-4 rounded-full p-4 text-2xl font-bold text-black"
+                  style={{ backgroundColor: feature.color }}
+                >
+                  <Image
+                    src={feature.icon}
+                    alt={"image"}
+                    width={32}
+                    height={32}
+                    className="object-contain"
+                  />
+                </div>
+                <p className="text-black text-sm font-medium">{feature.text}</p>
               </div>
-              <div className="text-white font-light text-sm">
-                {feature.text}
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-        {/* Daha fazla özellikler için metin */}
+
+        {/* Sağ taraf: Görsel */}
+        <div className="flex justify-center flex-1 order-1 md:order-2">
+          <img
+            src="/images/iphonemock2.png"
+            alt="Hakkımızda"
+            className="sm:h-[540px] sm:w-[360px] lg:h-[600px] lg:w-[400px] h-[300px] w-[200px]"
+          />
+        </div>
       </div>
     </section>
   );
